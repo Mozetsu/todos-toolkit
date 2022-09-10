6221b323c703fe1421ac57e7d73f6f9dbe7f066a
Mozetsu <hello@mozetsu.dev>
2022-09-10 21:53:32 +0100

    refactor Todos component

--- src/Todos.tsx
+++ src/Todos.tsx
@@ -1,11 +1,26 @@
-import { useAppDispatch, useAppSelector } from "./redux/hooks";
+import { useState } from "react";
 import { toggleTodo } from "./redux/todos";
+import { createTodo } from "./redux/todos";
 import type { Todo } from "./redux/todos";
 import type { Filter } from "./redux/filter";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
 
 function Todos() {
-	const { todos, filter } = useAppSelector((state) => state);
+	const [input, setInput] = useState<string>("");
+	const [indexCounter, setIndexCounter] = useState<number>(0);
+
+	// redux
 	const dispatch = useAppDispatch();
+	const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value);
+	const { todos, filter } = useAppSelector((state) => state);
+
+	const addTodo = () => {
+		if (!input.trim().length) return;
+
+		dispatch(createTodo({ id: indexCounter, text: input }));
+		setInput("");
+		setIndexCounter(indexCounter + 1);
+	};
 
 	const getVisibleTodos = (todos: Todo[], filter: Filter) => {
 		switch (filter) {
@@ -23,19 +38,23 @@ function Todos() {
 	const visibleTodos = getVisibleTodos(todos, filter);
 
 	return (
-		<ul>
-			{visibleTodos?.map((todo) => (
-				<li
-					style={{
-						textDecoration: todo.completed ? "line-through" : "initial",
-					}}
-					onClick={() => dispatch(toggleTodo(todo))}
-					key={todo.id}
-				>
-					{todo.text}
-				</li>
-			))}
-		</ul>
+		<>
+			<input value={input} type="text" placeholder="add todo..." onChange={(e) => handleInput(e)} />
+			<button onClick={() => addTodo()}>Add</button>
+			<ul>
+				{visibleTodos?.map((todo) => (
+					<li
+						style={{
+							textDecoration: todo.completed ? "line-through" : "initial",
+						}}
+						onClick={() => dispatch(toggleTodo(todo))}
+						key={todo.id}
+					>
+						{todo.text}
+					</li>
+				))}
+			</ul>
+		</>
 	);
 }
 
